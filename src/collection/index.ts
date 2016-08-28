fc6e89b41b627d45d28a1ac1a4748262b6fbb090
Jean-Matthieu BARBIER <jm.barbier@solidev.net>
2016-08-28 18:02:37 +0200

    [WIP] Added all index (hack to solve #2)

---
+++ src/collection/index.ts
@@ -0,0 +1,3 @@
+export * from "./interface";
+// export * from "./decorator";
+export * from "./collection";
---
+++ src/index.spec.ts
@@ -0,0 +1,7 @@
+import "./index";
+import {expect} from "chai";
+describe("ng2-dataobjects", () => {
+    it("should have all exports", () => {
+        expect(true).to.be.true;
+    });
+});
